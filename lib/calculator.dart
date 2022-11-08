d508c330be20810a525a58a53275aca75173f4d7
Usaid Asif <usaidasif12@gmail.com>
2022-11-08 18:04:30 +0500

    🚀  Uploading Project

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -10,8 +10,68 @@ class CalculatorApp extends StatefulWidget {
 }
 
 class _CalculatorAppState extends State<CalculatorApp> {
+  late int firstNum;
+  late int secondNum;
+  late String history = '';
+  late String textToDisplay = '';
+  late String res;
+  late String operation;
+
   void btnONclick(String btnVal) {
-    print(btnVal);
+    if (btnVal == 'C') {
+      textToDisplay = "";
+      firstNum = 0;
+      secondNum = 0;
+      res = '';
+    } else if (btnVal == 'AC') {
+      textToDisplay = "";
+      firstNum = 0;
+      secondNum = 0;
+      res = '';
+      history = '';
+    } else if (btnVal == '+/-') {
+      if (textToDisplay[0] != '-') {
+        res = '-$textToDisplay';
+      } else {
+        res = textToDisplay.substring(1);
+      }
+    } else if (btnVal == '<') {
+      res = textToDisplay.substring(0, textToDisplay.length - 1);
+    } else if (btnVal == '+' ||
+        btnVal == '-' ||
+        btnVal == 'X' ||
+        btnVal == '÷') {
+      firstNum = int.parse(textToDisplay);
+      res = '';
+      operation = btnVal;
+    } else if (btnVal == '=') {
+      secondNum = int.parse(textToDisplay);
+      if (operation == '+') {
+        res = (firstNum + secondNum).toString();
+        history =
+            firstNum.toString() + operation.toString() + secondNum.toString();
+      }
+      if (operation == '-') {
+        res = (firstNum - secondNum).toString();
+        history =
+            firstNum.toString() + operation.toString() + secondNum.toString();
+      }
+      if (operation == 'X') {
+        res = (firstNum * secondNum).toString();
+        history =
+            firstNum.toString() + operation.toString() + secondNum.toString();
+      }
+      if (operation == '÷') {
+        res = (firstNum / secondNum).toString();
+        history =
+            firstNum.toString() + operation.toString() + secondNum.toString();
+      }
+    } else {
+      res = int.parse(textToDisplay + btnVal).toString();
+    }
+    setState(() {
+      textToDisplay = res;
+    });
   }
 
   @override
@@ -25,7 +85,7 @@ class _CalculatorAppState extends State<CalculatorApp> {
             child: Padding(
               padding: const EdgeInsets.only(right: 12),
               child: Text(
-                '98x7',
+                history,
                 style: GoogleFonts.rubik(
                   textStyle: const TextStyle(
                     fontSize: 24,
@@ -40,7 +100,7 @@ class _CalculatorAppState extends State<CalculatorApp> {
             child: Padding(
               padding: const EdgeInsets.all(12),
               child: Text(
-                '686',
+                textToDisplay,
                 style: GoogleFonts.rubik(
                   textStyle: const TextStyle(
                     fontSize: 46,
@@ -109,7 +169,7 @@ class _CalculatorAppState extends State<CalculatorApp> {
                 textsize: 24,
               ),
               CalculatorButton(
-                text: 'x',
+                text: 'X',
                 textcolor: Colors.white,
                 callback: btnONclick,
                 color: Colors.deepPurpleAccent,
